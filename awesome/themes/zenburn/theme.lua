7800bc719203bf9dba706e9e66d4138add9d0048
Andreas Søvik <arsovik@gmail.com>
2013-05-20 13:20:46 +0200

    New Wallpaper

--- awesome/themes/zenburn/theme.lua
+++ awesome/themes/zenburn/theme.lua
@@ -9,7 +9,7 @@
 -- {{{ Main
 theme = {}
 --theme.wallpaper = "/usr/share/awesome/themes/zenburn/zenburn-background.png"
-theme.wallpaper = ".config/awesome/wallpapers/nature.jpg"
+theme.wallpaper = ".config/awesome/wallpapers/abstract-cool.jpg"
 -- }}}
 
 -- {{{ Styles
